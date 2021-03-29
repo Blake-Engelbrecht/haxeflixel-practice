c415df8d69f3547c2b377317fb89b0ed4c0b2b52
Blake Engelbrecht <bengelbrecht95@gmail.com>
2021-03-29 13:48:20 -0500

    added hello world text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,8 +5,12 @@ import flixel.FlxState;
 class PlayState extends FlxState
 {
 	override public function create()
-	{
+	{		
 		super.create();
+
+		var text = new flixel.text.FlxText(0, 0, 0, "Hello World!", 64);
+		text.screenCenter();
+		add(text);
 	}
 
 	override public function update(elapsed:Float)
